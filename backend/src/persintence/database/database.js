6c9ba4765fccddf339b21a28c89a1bae958c7f14
Axioos <axiaxos@gmail.com>
2026-09-14 15:19:57 -0300

    database.js a postgresql

--- backend/src/persintence/database/database.js
+++ backend/src/persintence/database/database.js
@@ -1,26 +1,16 @@
-// Backed /src / persistence /database /database.js
+// backend/src/persistence/database/database.js
 import Sequelize from "sequelize";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
-// export const sequelize = new Sequelize(
-//   "test",
-//   "root",
-//   "",
-  
-//   {
-//     host: "localhost",
-//     dialect: "mariadb",
-//   }
-// );
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT, 
-    dialect: "mariadb",
+    dialect: "postgres"
+    logging: false 
   }
 );
